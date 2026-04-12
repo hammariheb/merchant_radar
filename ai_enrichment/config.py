c4e3fc3a313f771d88efe6166325211fbd939da9
Iheb Hammar <iheb.hammar.officiel@gmail.com>
2026-04-12 16:47:11 +0200

    updating few other essential functionnalities in airflow

--- ai_enrichment/config.py
+++ ai_enrichment/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv(Path(__file__).parent.parent / ".env", override=True)
+load_dotenv(Path(__file__).parent.parent / ".env", override=False)
 
 
 def _require(key: str) -> str:
